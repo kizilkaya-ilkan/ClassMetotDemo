ae3c15c8a267cbd011b0d85d481bd74cb0284361
ilkan Kızılkaya <87038864+kizilkaya-ilkan@users.noreply.github.com>
2022-01-29 00:51:55 +0300

    Add files via upload

---
+++ ClassMetotDemo1/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo1
+{
+    internal class MusteriManager
+    {
+        public void Listeleme(Musteri[] GelenDeger)
+        {
+            foreach (var listeler in GelenDeger)
+            {
+                Console.WriteLine(listeler.Ad);
+            }
+        }
+
+        public void Ekle(Musteri Eklendi)
+        {
+            Console.WriteLine(Eklendi.Kidem);
+        }
+        
+        public void Sil(Musteri Silme)
+        {
+            Console.WriteLine(Silme.id);
+        }
+    }
+}
